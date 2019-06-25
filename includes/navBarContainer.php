f9badda9fbdc5bfd2af2a0a494b183023b4c710c
Laurens-Post <contact@lphosting.nl>
2019-06-25 13:45:40 +0200

    Added functions for the music player
    Added the following function for the music player:
    - Playlist of 10 random songs
    - Playing and pausing
    - Getting the song by AJAX
    - The counter for played songs
    - Remaining time
    - Volume bar, repeat, shuffle and skipping

--- includes/navBarContainer.php
+++ includes/navBarContainer.php
@@ -26,7 +26,7 @@
 			</div>
 
 			<div class="navItem">
-				<a href="profile.php" class="navItemLink">Laurens Post</a>
+				<a href="profile.php" class="navItemLink">Reece Kenney</a>
 			</div>
 		</div>
 
